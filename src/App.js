3a02dc3021db0d8cb39821dc66ca03c6baafbc5c
Vishwa <92839423+vishwaphansal@users.noreply.github.com>
2022-05-11 15:49:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <div>
             <div className="displayedNumber">{count}</div>
             <button type="button" class="btn btn-primary" onClick={() => setCount(count + 1)}>Count me!</button>
-            <button type="button" class="btn btn-primary" onClick={() => setCount(count - 1)}>Count me back!</button>
+         
             <button type="button" class="btn btn-primary" onClick={() => setCount(0)}>Reset</button>
            
         </div>
